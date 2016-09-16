b604a6aba716ab6a1c65578c83da2c5393366c83
Erik Bonn <ebonn89@gmail.com>
2016-09-15 20:36:25 -0700

    allergies release 4 added

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -13,6 +13,15 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you?(y
 puts "Would you like to enroll in the company’s health insurance?(y/n)"
 	health_insurance = gets.chomp
 
+puts "Name any allergies you have, one at a time, and enter done when finished/or if you have none."
+	allergies = gets.chomp
+	until allergies == "sunshine" || allergies == "done"
+		allergies = gets.chomp
+	end
+
+
+
+
 if name == "Drake Cula" || name == "Tu Fang" 
 	puts "Definitely a vampire"
 
@@ -22,7 +31,7 @@ elsif (birthday + age == 2016) && (wolves_like_garlic == "y" || health_insurance
 elsif (birthday + age != 2016) && (wolves_like_garlic == "n" && health_insurance == "n")
 	puts "almost certainly a vampire"
 
-elsif (birthday + age != 2016) && (wolves_like_garlic == "n" || health_insurance == "n")
+elsif (birthday + age != 2016) && (wolves_like_garlic == "n" || health_insurance == "n" || allergies == "sunshine")
 	puts "probably a vampire"	
 
 else
