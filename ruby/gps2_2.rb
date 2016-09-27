a0b69875e4b40a66cdb5ea8a3219f5f2c344e9c8
Erik Bonn <ebonn89@gmail.com>
2016-09-27 16:42:59 -0700

    finished release 2

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -25,9 +25,13 @@ p create_list("apples oranges milk")
 # Method to add an item to a list
 def add_item(item, quantity, list)
 	list[item] = quantity
+	item
 end
 
-p add_item("chicken", 2, grocery_list)
+p add_item("lemonade", 2, grocery_list)
+p add_item("tomatoes", 3, grocery_list)
+p add_item("oions", 1, grocery_list)
+p add_item("ice cream", 4, grocery_list)
 p grocery_list
 # input: item name and optional quantity
 # steps: accept 2 arguments (key, value) and assign that to the hash.
@@ -40,17 +44,37 @@ def remove_item(item, list)
 		i == item 
 	end
 end
-remove_item("apples", grocery_list)
+remove_item("lemonade", grocery_list)
 p grocery_list
 # steps: Use a hash method like deleteif to remove unwanted item from the hash
 # output: print updated has without the unwanted item.
 
 # Method to update the quantity of an item
 # input: would take 2 parameters. One for the item that they want to update and one for the new desired quantity.
+def update_item(item, quantity, list)
+	list.each do |i, q|
+		if list.has_key?(item)
+			list[item] = quantity
+		else 
+			puts "List does not contain that item."
+			break
+		end
+	end
+end
+
+update_item("ice cream", 1, grocery_list)
+p grocery_list
+
 # steps: iterate through the hash to see if the item is present, and if so, reassign the value. If item not present, we could print an error message or call the add method that have made.
 # output: print out new key and value.
 
 # Method to print a list and make it look pretty
-# input: none
+# input: accepts grocery list
+def print_list(list)
+	list.each do |item, quantity|
+		puts "There are #{quantity} #{item} on the grocery list!!"
+	end
+end	
+print_list(grocery_list)
 # steps: iterate over the hash to print out the key and value pairs in a pretty way.
 # output: string with the associated key and value pairs.
