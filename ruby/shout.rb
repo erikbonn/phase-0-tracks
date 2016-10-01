d40271075ec22ec6b88d60e26f5990044cbb697f
Erik Bonn <ebonn89@gmail.com>
2016-10-01 15:11:49 -0700

    mixin applied to two classes with shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,39 @@
-module Shout
-  # we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# # A simple standalone module
+# module Shout
+#   # we'll put some methods here soon, but this code is fine for now!
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(phrase)
-  	phrase + "!!!" + ":)"
-  end
+#   def self.yelling_happily(phrase)
+#   	phrase + "!!!" + " :)"
+#   end
+# end
+
+# words = "MAMA MIA"
+# phrase = "YASSSS"
+# p Shout.yell_angrily(words)
+# p Shout.yelling_happily(phrase)
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yelling_happily(phrase)
+		phrase + "!!!" + " :)"
+	end
 end
 
-words = "MAMA MIA"
-phrase = "YASSSS"
-p Shout.yell_angrily(words)
-p Shout.yelling_happily(phrase)
+class Choir
+	include Shout
+end
+
+class Warrior
+	include Shout
+end
+
+choir = Choir.new
+p choir.yelling_happily("MAMA MIA")
+warrior = Warrior.new
+p warrior.yell_angrily("YASSS DESTROY")
