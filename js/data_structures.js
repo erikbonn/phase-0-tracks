14c2ee4543fd49661aef098936a2d5ae81707ae2
Erik Bonn <ebonn89@gmail.com>
2016-10-08 20:16:33 -0700

    added a loop to output our key value pairs for the car object and added some comments at the end

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,10 +6,41 @@ horseColor.push("purple");
 console.log(horseColor);
 horseName.push("Seabiscuit");
 console.log(horseName);
-
-var horses = {horseName: 'horseColor'};
-console.log(horses);
-
 for (var i = 0; i < horseName.length && horseColor.length; i ++){
-	console.log(horseName[i] + " " + horseColor[i]);
+    console.log(horseName[i] + " " + horseColor[i]);
 }
+function Car(brand, model, preowned) {
+    console.log("Our new car is:", this);
+    this.brand = brand;
+    this.model = model;
+    this.preowned = preowned;
+    this.revengine = function() { console.log("Vroooom!!");};
+}
+console.log("Let's build us a car!");
+var firstCar = new Car("Chevy", "Silverado", false);
+console.log(firstCar);
+console.log("Rev the engine and listen to that baby purrrrrrr.");
+firstCar.revengine();
+for (var key in firstCar) {
+    if (firstCar.hasOwnProperty(key)) {
+        console.log(key + "->" + firstCar[key])
+    }
+}
+
+var secondCar = new Car("Cadillac", "XLR", true);
+console.log(secondCar);
+console.log("Rev the engine and listen to that baby purrrrrrr.");
+secondCar.revengine();
+for (var key in secondCar) {
+    if (secondCar.hasOwnProperty(key)) {
+        console.log(key + "->" + secondCar[key])
+    }
+}
+
+///// release 3: We looped through the keys and values of the firstcar and secondcar object using a for loop we found through research.
+//    using a contstructer function to make a car is useful, but it seems like you use a constructor when you need to add
+//    methods and more things to it. If you just need something to store some items, you can you an object literal. 
+
+
+
+
