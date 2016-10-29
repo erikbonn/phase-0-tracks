7783f3cb0e91bd0cd6dfa1a5ada5411b146dd39b
Erik Bonn <ebonn89@gmail.com>
2016-10-28 17:59:43 -0700

    created a decrpyt method and driver code to test it all

--- ruby/secret_agents_otters.rb
+++ ruby/secret_agents_otters.rb
@@ -1,5 +1,5 @@
 # obtain a secret word string from user
-def Encrypt(secret_word)
+def encrypt(secret_word)
 	# get each individual letter in the string
 	i = 0 
 	while i < secret_word.length
@@ -13,5 +13,36 @@ def Encrypt(secret_word)
 			secret_word[i] = "z"
 		end
 	i += 1
-
 	end
+	secret_word # needs to return the word
+end 
+
+def decrypt(secret_word)
+	#compare the word to the alphabet
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	i = 0
+	while i < secret_word.length
+		# take into account the spaces and z's
+		if secret_word[i] != " " && secret_word[i] != "z"
+			secret_word[i] = alphabet[(alphabet.index(secret_word[i]) - 1 )]
+		elsif secret_word[i] == " "
+			secret_word[i] = " "
+		elsif secret_word[i] == "z"
+			secret_word[i] = "z"
+		end 
+	i += 1
+	end
+	secret_word
+end
+
+puts "please enter secret_word:"
+secret_word = gets.chomp
+puts "would you like to encrypt or decrypt? (e/d)" 
+choice = gets.chomp
+if choice == "e" 
+		p encrypt(secret_word)
+	elsif choice == "d"
+		p decrypt(secret_word)
+end
+#returned swordfish because it encrypted it and then decrypted it.
+p decrypt(encrypt("swordfish"))
