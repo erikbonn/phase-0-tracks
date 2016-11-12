3bb387e3d47b00a4b4dbd9168e60e3e3374fe585
Erik Bonn <ebonn89@gmail.com>
2016-11-11 17:19:39 -0800

    created mixin modules for hacker and viking classes

--- ruby/shout_otters.rb
+++ ruby/shout_otters.rb
@@ -1,14 +1,41 @@
+# module Shout
+
+# 	def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#  	end
+
+#  	def self.yell_happily(other_word)
+#  		other_word + "yayyyy!"
+#  	end
+
+# end
+
+# p Shout.yell_happily("sucess!!! ")
+# p Shout.yell_angrily("BOOOOOOO ")
+
 module Shout
 
-	def self.yell_angrily(words)
-    words + "!!!" + " :("
+	def yell_angrily(words)
+    	words + "!!!" + " :("
  	end
 
- 	def self.yell_happily(other_word)
+ 	def yell_happily(other_word)
  		other_word + "yayyyy!"
  	end
 
 end
 
-p Shout.yell_happily("sucess!!! ")
-p Shout.yell_angrily("BOOOOOOO ")
+class Viking
+	include Shout
+end
+
+class Hacker
+	include Shout
+end
+
+erik = Viking.new
+p erik.yell_angrily("plunder and conquer ")
+p erik.yell_happily("I discovered America ")
+dexter = Hacker.new
+p dexter.yell_happily("My code works! ")
+p dexter.yell_angrily("But now I have 10 other problems ")
