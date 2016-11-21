b55a6c7e1958a7f4e559069e86d0a27e6ed13425
Erik Bonn <ebonn89@gmail.com>
2016-11-21 09:21:58 -0800

    added function that identifies if there is a key value match between two objects

--- js/algos_otters.js
+++ js/algos_otters.js
@@ -9,17 +9,54 @@ var words = ["long phrase","longest phrase","longer phrase"];
 var peeps = ["jeff", "jillian", "jacob", "jalapeno"]
 
 function longestWord(array) {
-	longest = 0;
-	longestword = "";
-	for (var i = 0; i < array.length; i++) {
-		if (array[i].length > longest && typeof array[i] =="string") {
-			longest = array[i].length;
-			longestword[i] = array[i];
+    var longest = 0;
+    var longestWord = "";
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].length > longest && typeof array[i] ==="string") {
+        longest = array[i].length;
+        longestWord = array[i];
+        }
+    }
+    return longestWord;
+}
+
+//// release 1
+//input: two objects
+// output: true if they have one key/value pair that matches, false if otherwise
+// function pseudocode:
+//     SET objects_match = FALSE
+//     FOR every key in object1:
+//     IF the key is also in object2:
+//         IF object1 key's value is equal to object2 key's value:
+//             RETURN TRUE
+//         ELSE go on to next key in object1
+//     ELSE go on to next key in object1
+  
+//     return FALSE (assuming there's been no match to trigger TRUE)
+
+var people1 = {name: "Steven", age: 54}
+var people2 = {name: "Tamir", age: 54}
+var cats1 = {type: "siamese", hair: "short"}
+var cats2 = {type: "tabby", hair: "orangey"}
+
+function share(obj1, obj2) {
+	for (var key in obj1) {
+		if (obj2.hasOwnProperty(key)) {
+			if (obj2[key] == obj1[key]) {
+				return true;
+			}
 		}
 	}
-	return longestword;
+	return false;
 }
+
 
 console.log(longestWord(words));
 console.log(words);
 console.log(longestWord(peeps));
+console.log(share(people1, people2));
+console.log(share(cats1, cats2));
+
+
+
+
