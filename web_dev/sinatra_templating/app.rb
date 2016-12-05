4429d1c2b578e110d771ed843f4c29951b7f3f2d
Erik Bonn <ebonn89@gmail.com>
2016-12-05 09:54:56 -0800

    tried to add the ability to add a major to the new student form

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -13,15 +13,25 @@ get '/' do
   erb :home
 end
 
+# get '/students/new' do
+#   erb :new_student
+# end
+
 get '/students/new' do
-  erb :new_student
+  erb :major
 end
 
 # create new students via
 # a form
 post '/students' do
-  db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
+  db.execute("INSERT INTO students (name, campus, age, major) VALUES (?,?,?,?)", [params['name'], params['campus'], params['age'].to_i], params['major'])
   redirect '/'
 end
 
+#### My issue with this is that I need to add a major column to the db for the major names to be stored. I think everything else looks ok though, I'm out 
+#### of time, but I can come back later to figure that out.
+
 # add static resources
+
+
+
